3652af026f7c7259daac7a57f2b16731968b4c8a
Santiago Piccoli <121751727+spiccoli@users.noreply.github.com>
2025-05-09 19:50:06 -0300

    Comparable

---
+++ prog2/9-05/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Movie> myMovie = new ArrayList<Movie>();
+
+        Movie movie1 = new Movie(44);
+        myMovie.add(movie1);
+
+        Movie movie2 = new Movie(50);
+        myMovie.add(movie2);
+
+        Movie movie3 = new Movie(30);
+        myMovie.add(movie3);
+
+        Collections.sort(myMovie);
+
+        for (Movie movie : myMovie) {
+            System.out.println(movie.getYear());
+        }
+    }
+}
---
+++ prog2/9-05/src/Movie.java
@@ -0,0 +1,21 @@
+public class Movie implements Comparable<Movie> {
+//Attributes.
+    private int year;
+    public int getYear() {
+        return year;
+    }
+//Constructor.
+    public Movie(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public int compareTo(Movie m) {
+        return Integer.compare(this.year, m.year);
+    }
+
+    @Override
+    public String toString() {
+        return "Movie(" + year + ")";
+    }
+}
